0b870d246137db2c5a2ed633c76a4aece6863b50
mehtaVatsalD <mehtavatsald02@gmail.com>
2021-06-28 09:38:59 +0530

    adding code to find middle in link list

---
+++ educative/3FastSlowPointers/MiddleLinkNode.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class MiddleLinkNode {
+
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		Node first = sol.takeInput();
+		Node middleNode = getMiddleNodeInLinkList(first);
+		System.out.println(middleNode.val);
+	}
+	
+	private static Node getMiddleNodeInLinkList(Node first) {
+		Node fast = first;
+		Node slow = first;
+		while(fast.next != null) {
+			fast = fast.next;
+			slow = slow.next;
+			if(fast.next != null) {
+				fast = fast.next;
+			}
+		}
+		return slow;
+	}
+
+}
